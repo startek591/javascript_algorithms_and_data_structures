b0cd3622ffd14d93836838966e701e5318a9c83b
Jonathan Fambro <startek591@gmail.com>
2024-01-29 15:01:12 -0500

    Are all chars unqiue

---
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -0,0 +1,17 @@
+function areAllCharactersUnique(str) {
+  const charSet = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (charSet.has(char)) {
+      return false;
+    }
+
+    charSet.add(char);
+  }
+
+  return true;
+}
+
+module.exports = areAllCharactersUnique;
