1cca0f76f68c9cf0324e470e29d20704d6e5a9dc
Jonathan Fambro <startek591@gmail.com>
2024-01-29 13:36:32 -0500

    Find missing letter

---
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter-run.js
@@ -0,0 +1,3 @@
+const findMissingLetter = require("./find-missing-letter");
+result = findMissingLetter(["a", "b", "c", "d", "f", "g"]);
+console.log(result);
---
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.test.js
@@ -0,0 +1,7 @@
+const findMissingLetter = require("./find-missing-letter");
+
+test("Find Missing Letter", () => {
+  expect(findMissingLetter(["a", "b", "c", "e"])).toBe("d");
+  expect(findMissingLetter(["X", "Z"])).toBe("Y");
+  expect(findMissingLetter(["F", "G", "H", "J"])).toBe("I");
+});
